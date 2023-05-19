badb515f973559cb42eb2704f52dae93da91c9b0
Rahul Singh <55955478+singhrahul7874@users.noreply.github.com>
2023-05-19 15:31:51 +0530

    Update used_cars_mysql_script.sql

--- Data_Cleaning/used_cars_mysql_script.sql
+++ Data_Cleaning/used_cars_mysql_script.sql
@@ -1,74 +1,67 @@
-use  used_cars;
-select * from used_car_dataset;
+-- Switching to the "used_cars" database and selecting all records from the "used_car_dataset" table
+USE used_cars;
+SELECT * FROM used_car_dataset;
 
+-- Removing safe update mode to allow updates without a key constraint
+SET SQL_SAFE_UPDATES = 0;
 
-SET SQL_SAFE_UPDATES=0;
-update used_car_dataset
-set kms_driven = replace(kms_driven,"km","");
-update used_car_dataset
-set kms_driven = replace(kms_driven,",","");
+-- Removing unwanted characters from the "kms_driven" column
+UPDATE used_car_dataset
+SET kms_driven = REPLACE(kms_driven, "km", "");
+UPDATE used_car_dataset
+SET kms_driven = REPLACE(kms_driven, ",", "");
 
+-- Removing unwanted characters and converting car prices to standard format in the "car_price_in_rupees" column
+UPDATE used_car_dataset
+SET car_price_in_rupees = REPLACE(car_price_in_rupees, "â‚¹ ", "");
+UPDATE used_car_dataset
+SET car_price_in_rupees = CASE 
+    WHEN car_price_in_rupees LIKE "%Lakh" THEN REPLACE(car_price_in_rupees, " Lakh", "") * 100000
+    WHEN car_price_in_rupees LIKE "%Crore" THEN REPLACE(car_price_in_rupees, " Crore", "") * 10000000
+    ELSE car_price_in_rupees
+  END;
+UPDATE used_car_dataset
+SET car_price_in_rupees = ROUND(car_price_in_rupees);
 
-update used_car_dataset
-set car_price_in_rupees = replace(car_price_in_rupees,"â‚¹ ", "");
+-- Modifying the data type of the "car_price_in_rupees" column to INT
+ALTER TABLE used_car_dataset MODIFY COLUMN car_price_in_rupees INT;
 
-update used_car_dataset
-set car_price_in_rupees = case when car_price_in_rupees like "%Lakh" 
-then replace(car_price_in_rupees," Lakh","")*100000
-when car_price_in_rupees like "%Crore"
-then replace(car_price_in_rupees," Crore","")*10000000
-else car_price_in_rupees
-end;
+-- Adding "brand" and "car_model" columns to the table
+ALTER TABLE used_car_dataset ADD COLUMN brand VARCHAR(255);
+ALTER TABLE used_car_dataset ADD COLUMN car_model VARCHAR(255);
 
-update used_car_dataset
-set car_price_in_rupees = round(car_price_in_rupees);
+-- Populating the "brand" and "car_model" columns based on the car name
+UPDATE used_car_dataset
+SET brand = CASE 
+    WHEN car_name LIKE "Maruti Suzuki%" THEN "Maruti Suzuki"
+    WHEN car_name LIKE "Land Rover%" THEN "Land Rover"
+    ELSE SUBSTRING_INDEX(car_name, " ", 1)
+  END;
+UPDATE used_car_dataset
+SET car_model = REPLACE(car_name, brand, "");
 
+-- Trimming the whitespace in the "car_model" column
+UPDATE used_car_dataset
+SET car_model = TRIM(car_model);
 
-Alter table used_car_dataset Modify column car_price_in_rupees INT;
+-- Dropping the "car_name" column
+ALTER TABLE used_car_dataset DROP COLUMN car_name;
 
--- Car_name 
+-- Retrieving records where the car_model starts with 'u' or 'n'
+SELECT *
+FROM used_car_dataset
+WHERE LEFT(car_model, 1) IN ("u", "n");
 
--- Brand - 
--- Model -
+-- Retrieving records where the car_model starts with 'u' or 'n' using regular expression
+SELECT *
+FROM used_car_dataset
+WHERE car_model REGEXP "^[un]";
 
--- Maruti Suzuki
--- Land Rover
+-- Retrieving records where the car_model ends with 'uncopyrightable' using regular expression
+SELECT *
+FROM used_car_dataset
+WHERE car_model REGEXP "[uncopyrightable]$";
 
--- car_price_in_rupees
-
-Alter table used_car_dataset add column brand Varchar(255);
-Alter table used_car_dataset add column car_model Varchar(255);
-
--- Maruti Suzuki
--- Land Rover
-
-update used_car_dataset
-set brand = case 
-	when car_name like "Maruti Suzuki%" then "Maruti Suzuki"
-    when car_name like "Land Rover%" then "Land Rover"
-    else substring_index(car_name," ",1)
-end;
-
-update used_car_dataset
-set car_model = replace(car_name,brand,"");
-
-Select * from used_car_dataset;
-
-update used_car_dataset
-set car_model = trim(car_model);
-
-Alter table used_car_dataset drop column car_name;
-
--- "uncopyrightable"
-
-Select * from used_car_dataset where left(car_model,1)  in ("u","n") ;
-
-
-Select * from used_car_dataset
-where car_model Regexp "^[un]";
-
-Select * from used_car_dataset
-where car_model Regexp "[uncopyrightable]$";
-
-Select Regexp_Replace(car_model,"[^a-zA-Z]","") as car_model
-from used_car_dataset;
+-- Removing non-alphabetic characters from the car_model column using regular expression
+SELECT REGEXP_REPLACE(car_model, "[^a-zA-Z]", "") AS car_model
+FROM used_car_dataset;
